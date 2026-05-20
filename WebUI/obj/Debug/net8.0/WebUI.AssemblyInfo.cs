e6eea3fe06f08c3f91757a1ff9ac121f5cd4c26d
mvdevelop <marcosvmdilly@gmail.com>
2026-05-20 16:27:11 -0300

    feat: adding data

--- WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3093e058060b348cbaad8ff49abf7591b6b2cf81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e552d83de9961a5efd40be91ed9a730b22f03a1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
